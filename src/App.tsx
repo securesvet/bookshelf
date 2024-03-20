51445d01464c74b3af00a9142a0077fdaa41e39e
Sviatoslav <mursvyats@gmail.com>
2024-03-20 21:42:45 +0500

    Added base code for input suggestive field

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useRef, useState } from "react";
+import './App.css'
 function App() {
+  const changeValue = () => {
+    setSearchInputValue(searchInputRef.current?.value)
+  }
 
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const [searchInputValue, setSearchInputValue] = useState<String | undefined>('');
+  const arrayOfWords = ["hello", "hi", "hey", "to kill a mokingbird", "divine comedy"]
+  const [suggestions, setSuggestions] = useState<String[]>([])
+
+  useEffect(() => {
+    const filteredSuggestions = arrayOfWords.filter(word =>
+      word.toLowerCase().includes(searchInputValue!.toLowerCase())
+    )
+    setSuggestions(filteredSuggestions)
+  })
   return (
     <>
-     <input placeholder="Hello world!"/>
+    <div className="input-container">
+     <input onChange={changeValue} ref={searchInputRef} placeholder="Hello world!"/>
+     <div className="suggestions-container">
+        {suggestions.map((suggestion, index) => (
+                <div key={index}>{suggestion}</div>
+              ))}
+     </div>
+    </div>
     </>
   )
 }
